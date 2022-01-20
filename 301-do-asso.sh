74ce11ff2807b49308e01b4b79dfd4e1d62f3a08
maccampbell <campbellmatthewa@gmail.com>
2022-01-20 08:24:13 -0500

    Specifying correct partition

--- 301-do-asso.sh
+++ 301-do-asso.sh
@@ -21,7 +21,7 @@ while read chrom; do
   -doMajorMinor 1 -doMaf 1 -SNP_pval 1e-6 -r $chrom -out $chrom-asso \
   -bam $bamlist  > $chrom-asso.out 2> $chrom-asso.err " > $chrom-asso.sh
   
-sbatch -p bigmem -t 24:00:00 --mem=32G --nodes=2 $chrom-asso.sh
+sbatch -p bigmemm -t 24:00:00 --mem=32G --nodes=2 $chrom-asso.sh
 
 done < $list
 
