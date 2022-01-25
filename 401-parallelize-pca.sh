e96edc1415001c8697d84c3d32eeeb5fd277a236
maccampbell <campbellmatthewa@gmail.com>
2022-01-25 16:18:51 -0500

    -postCutoff is for genotype calling!

--- 401-parallelize-pca.sh
+++ 401-parallelize-pca.sh
@@ -4,6 +4,9 @@
 # Feeding a list of chroms meta/test.chroms
 # bash $HOME/shernook/201-parallelize-analyses.sh $HOME/shernook/bamlists/bamlist56.bamlist $HOME/shernook/meta/test.chroms
 
+## Example for dsm-omics
+# bash $HOME/dsm-omics/401-parallelize-pca.sh $HOME/dsm-omics/bamlists/test96.bamlist $HOME/dsm-omics/meta/lgs.txt 
+
 
 bamlist=$1
 list=$2
@@ -16,7 +19,7 @@ while read chrom; do
   echo "#!/bin/bash -l
   $HOME/angsd/angsd -nThreads 6 -minInd $thresh -GL 1 \
 -doGlf 2  -doMajorMinor 1 -doMaf 2 -SNP_pval 1e-6 -minMapQ 20 -minQ 20 \
--r $chrom -out $chrom-pca -minMaf 0.05 -postCutoff 0.90 \
+-r $chrom -out $chrom-pca -minMaf 0.05 \
 -bam $bamlist > $chrom.out 2> $chrom.err " > $chrom.sh
   
 sbatch -p bigmemm -t 12:00:00 --mem=32G --nodes=1 $chrom.sh
