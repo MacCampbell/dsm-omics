e8ac130e03188d565ceeab664219b4f64acf7778
maccampbell <campbellmatthewa@gmail.com>
2022-07-22 09:21:35 -0800

    Estimating per chromosome fst

--- 701.1-sfs.sh
+++ 701.1-sfs.sh
@@ -1,16 +1,35 @@
 #! /bin/bash
 
 # Basic script to generate Fst data
-# Can execute with srun 
+# Can execute with sbatch
 
-realSFS outputs/701/hdi.saf.idx >  outputs/701/hdi.sfs;
-realSFS outputs/701/ldi.saf.idx >  outputs/701/ldi.sfs;
-realSFS outputs/701/hdi.saf.idx outputs/701/ldi.saf.idx > outputs/701/hdi-ldi.2dsfs;
-realSFS fst index  outputs/701/hdi.saf.idx  outputs/701/ldi.saf.idx -sfs outputs/701/hdi-ldi.2dsfs -fstout outputs/701/hdi-ldi;
+# Usage
+# 701.1-sfs.sh chromlist.txt
+
+chromlist=$1
+
+while read chrom; do
+  echo "#!/bin/bash -l
+realSFS outputs/701/hdi.saf.idx -r $chrom >  outputs/701/$chrom-hdi.sfs;
+realSFS outputs/701/ldi.saf.idx -r $chrom >  outputs/701/$chrom-ldi.sfs;
+realSFS outputs/701/hdi.saf.idx -r $chrom  outputs/701/ldi.saf.idx -r $chrom  > outputs/701/$chrom-hdi-ldi.2dsfs;
+realSFS fst index  outputs/701/hdi.saf.idx  outputs/701/ldi.saf.idx -r $chrom -sfs outputs/701/$chrom-hdi-ldi.2dsfs -fstout outputs/701/$chrom-hdi-ldi;
 
 # Global estimate
-realSFS fst stats outputs/701/hdi-ldi.fst.idx > outputs/701/hdi-ldi.fst.stats;
+realSFS fst stats outputs/701/$chrom-hdi-ldi.fst.idx > outputs/701/$chrom-hdi-ldi.fst.stats;
 
 # Sliding window
 
-realSFS fst stats2  outputs/701/hdi-ldi.fst.idx -win 50000 -step 10000 >  outputs/701/sliding-window-50k-10k
+realSFS fst stats2  outputs/701/$chrom-hdi-ldi.fst.idx -win 50000 -step 10000 >  outputs/701/$chrom-sliding-window-50k-10k
+
+
+ " > $chrom-fst.sh
+  
+sbatch -p med -t 24:00:00 --mem=16G --nodes=1 $chrom-fst.sh
+
+done < $chromlist
+
+
+
+
+
