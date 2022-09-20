c2cfc0ae311208c7bf0596851272249af98a6ebf
maccampbell <campbellmatthewa@gmail.com>
2022-09-19 21:43:12 -0800

    Working through aligning Tagseq with STAR

--- 1100-methyl.sh
+++ 1100-methyl.sh
@@ -6,9 +6,9 @@
 ## QC
 sbatch -p med -J fastqc.$USER --array=1-133 fastqc_pre.slurm; # restarted 9/19
 sbatch -p high -J mqc.${USER} multiqc_pre.slurm;
-sbatch -p med -J tg.${USER} --array=1-133 trimgalore.slurm; # started 9/19
-jobid=$(sbatch -p bigmemm -J fqcp.${USER} --array=1-133 fastqc_post.slurm |cut -d' ' -f4 - )
+sbatch -p med -J tg.${USER} --array=1-133 trimgalore.slurm; # started 9/19, done
+jobid=$(sbatch -p med -J fqcp.${USER} --array=1-133 fastqc_post.slurm |cut -d' ' -f4 - )
 sbatch -J mqcp.${USER} --dependency=afterok:${jobid} multiqc_post.slurm
 
 ## Align
-sbatch -p med -J bm1.${USER} --array=1-133 bismark_part1.slurm
+sbatch -p med -J bm1.${USER} --array=1-133 bismark_part1.slurm # started 9/19, failed
