8d1d28f97e2670592b474c450e9e8301277520e4
maccampbell <campbellmatthewa@gmail.com>
2022-11-29 13:15:47 -0900

    Worked through methylation analysis with bismark

--- 1100-methyl.sh
+++ 1100-methyl.sh
@@ -10,5 +10,15 @@ sbatch -p med -J tg.${USER} --array=1-133 trimgalore.slurm; # started 9/19, done
 jobid=$(sbatch -p med -J fqcp.${USER} --array=1-133 fastqc_post.slurm |cut -d' ' -f4 - )
 sbatch -J mqcp.${USER} --dependency=afterok:${jobid} multiqc_post.slurm
 
-## Align
+## Align and get Bismark metrics
 sbatch -p med -J bm1.${USER} --array=1-133 bismark_part1.slurm # appears to partially finish, needs to generate final step
+sbatch -p med -J bm2.${USER} --array=1-133 bismark_part2.slurm 
+
+## Compile metrics
+ls $HOME/Methylation/03-Bismark/*_PE_report.txt > input2.fofn
+ls $HOME/Methylation/03-Bismark/*.deduplication_report.txt >> input2.fofn
+ls $HOME/Methylation/04-Methylation/*.M-bias.txt >> input2.fofn
+ls $HOME/Methylation/04-Methylation/*_splitting_report.txt >> input2.fofn
+sbatch -J mqb.${USER} multiqc_bismark.slurm
+
+sbatch -J cnt.${USER} --array=1-133 gather_rawcounts.slurm
